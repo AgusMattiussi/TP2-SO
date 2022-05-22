cb62968129ed6f12f7d7f3fa452bff6e93cdccb8
Julian Sasso <jsasso@itba.edu.ar>
2022-05-22 20:02:15 -0300

    enqProcess, deqProcess, createProcess

--- Kernel/c/scheduler.c
+++ Kernel/c/scheduler.c
@@ -1,6 +1,7 @@
 #include <types.h>
 
 #define NAME_MAX_SIZE 25
+#define PROCESS_STACK_SIZE 0x1000
 
 typedef enum {READY, BLOCKED, KILLED} states;
 
@@ -29,7 +30,7 @@ typedef struct stackFrame{
 
 typedef struct processContext{
     char name[NAME_MAX_SIZE];
-    uint64_t pid;
+    pid_t pid;
     uint64_t rsp;
     uint64_t rbp;
     // uint64_t priority;
@@ -49,6 +50,84 @@ typedef struct processList{
     int readyCount;
 } processList;
 
-static uint64_t lastGivenPid = 1;
-static processList currentList;
+static pid_t lastGivenPid = 1;
+static processList * currentList;
 static process * executingP;
+
+static void enqProcess(process * pr) {
+
+    /* Si la lista no existe o el proceso es nulo, no puedo encolarlo */
+    if(pr == NULL || currentList == NULL)
+        return;
+    
+    /* Si no habia procesos en la lista, este es el primero */
+    if(currentList->first == NULL)
+        currentList->first = pr;
+    /* Si no, es el nuevo ultimo */
+    else
+        currentList->last->next = pr;
+    
+    /* Actualizo el puntero al ultimo */
+    currentList->last = pr;
+    /* Como es lista circular, el siguiente del ultimo es el primero */
+    pr->next = currentList->first;
+
+    /* Por default asumimos que el proceso se crea en READY */
+    currentList->readyCount++;
+    currentList->size++;
+}
+
+static process * deqProcess() {
+
+    /* Si la lista no existe o el proceso es nulo, no puedo sacarlo */
+    if(currentList == NULL || currentList->size == 0)
+        return NULL;
+
+    /* Tomo el primero (asumo FIFO). No puede ser NULL */
+    process * deq = currentList->first;
+
+    /* Si habia un solo elemento, entonces first == last. Seteo ambos en
+     * NULL, pues ahora la lista quedara vacia */
+    if(currentList->size == 1){
+        currentList->first = NULL;
+        /* Creo que es redundante, porque first==last */
+        currentList->last = NULL;
+    } else {
+        /* Si no, el first sera el siguiente al que voy a sacar */
+        currentList->first = deq->next;
+        /* Como habia sacado first, el siguiente de last quedo NULL. Lo actualizo */
+        currentList->last->next = currentList->first;
+    }
+
+    /* Elimino la referencia al siguiente */
+    deq->next = NULL;
+
+    /* Si estaba READY, actualizo readyCount*/
+    if(deq->pc.state == READY)
+        currentList->readyCount--;
+
+    currentList->size--;
+    return deq;
+}
+
+pid_t createProcess(void (*pFunction)(int, char **), int argc, char **argv){
+
+    //TODO: Cambiar a malloc a secas
+    process * new = b_malloc(sizeof(process) + PROCESS_STACK_SIZE);
+    if(new == NULL)
+        return 0;
+
+    char * prName = argv[0];
+    if(initProcess(new, prName) == 0)
+        return 0;
+
+    //TODO: Cambiar a malloc a secas
+    char **prArgs = b_malloc(sizeof(char *) * argc)
+    if(prArgs == NULL)
+        return 0;
+
+    //TODO: Implementar copyArguments
+    //TODO: Llamar a initializeStackFrame
+    enqProcess(new);
+    return new->pc.pid;
+}
--- Kernel/include/types.h
+++ Kernel/include/types.h
@@ -10,5 +10,6 @@ typedef unsigned short uint16_t;
 typedef unsigned int uint32_t;
 typedef unsigned long long uint64_t;
 typedef unsigned long long size_t;
+typedef unsigned long long pid_t 
 
 #endif
